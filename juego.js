fd30c4ca3eb681ea990f1fb2bb3c91c181246e40
Florencia <florenciademollein@gmail.com>
2020-09-25 18:03:59 -0300

    Colores de piezas y márgenes del tablero

--- juego.js
+++ juego.js
@@ -8,15 +8,22 @@ var altoCanvas = 640;
 
 // Medidas reales del tablero
 var anchoTablero = 10;
-var altoTablero = 16;
+var altoTablero = 20;
+
+// filas superiores donde aparecerá la nueva pieza, pero permanecerán ocultas en el dibujo del tablero (también habrá margen lateral de 1)
+var margenSuperior = 4;
 
 // Medidas en px de cada recuadro
 var anchoFicha = 40;
 var altoFicha = 40;
 
-// Dimensiones reales del tablero: 12 x 17
-// Se verá en pantalla: 10 x 16
+// Dimensiones reales del tablero: 12 x 21
+// Se verá en pantalla: 10 x 20
 var tablero = [
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+  [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
@@ -35,6 +42,15 @@ var tablero = [
   [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
   [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ];
+
+// COLORES
+var rojo = "#FF0000";
+var morado = "#800080";
+var naranja = "#FF8C00";
+var amarillo = "#FFD700";
+var verde = "#008000";
+var cyan = "#00CED1";
+var azul = "#0000CD";
 
 //DIBUJO DE LAS PIEZAS (Matriz de 4 dimensiones)
 // fichaGrafico [Pieza] [Posición/rotación] [y] [x]
@@ -261,7 +277,21 @@ var objPieza = function () {
     for (py = 0; py < 4; py++) {
       for (px = 0; px < 4; px++) {
         if (fichaGrafico[this.tipo][this.angulo][py][px] != 0) {
-          ctx.fillStyle = "#800080";
+          if (fichaGrafico[this.tipo][this.angulo][py][px] == 1)
+            ctx.fillStyle = rojo;
+          if (fichaGrafico[this.tipo][this.angulo][py][px] == 2)
+            ctx.fillStyle = naranja;
+          if (fichaGrafico[this.tipo][this.angulo][py][px] == 3)
+            ctx.fillStyle = amarillo;
+          if (fichaGrafico[this.tipo][this.angulo][py][px] == 4)
+            ctx.fillStyle = verde;
+          if (fichaGrafico[this.tipo][this.angulo][py][px] == 5)
+            ctx.fillStyle = cyan;
+          if (fichaGrafico[this.tipo][this.angulo][py][px] == 6)
+            ctx.fillStyle = azul;
+          if (fichaGrafico[this.tipo][this.angulo][py][px] == 7)
+            ctx.fillStyle = morado;
+
           ctx.fillRect(
             (this.x + px) * anchoFicha,
             (this.y + py) * altoFicha,
@@ -289,6 +319,38 @@ var objPieza = function () {
     console.log("izq");
   };
 };
+
+// DIBUJAR TABLERO
+function dibujaTablero() {
+  // No empieza en 0 sino en 4, para ocultar nuevas fichas
+  for (py = margenSuperior; py < altoTablero; py++) {
+    // Empieza en 1 para ocultar margen izquierdo
+    for (px = 1; px < anchoTablero; px++) {
+      if (tablero[py][px] != 0) {
+        if (tablero[py][px] == 1) ctx.fillStyle = rojo;
+
+        if (tablero[py][px] == 2) ctx.fillStyle = naranja;
+
+        if (tablero[py][px] == 3) ctx.fillStyle = amarillo;
+
+        if (tablero[py][px] == 4) ctx.fillStyle = verde;
+
+        if (tablero[py][px] == 5) ctx.fillStyle = cyan;
+
+        if (tablero[py][px] == 6) ctx.fillStyle = azul;
+
+        if (tablero[py][px] == 7) ctx.fillStyle = morado;
+
+        ctx.fillRect(
+          (px - 1) * anchoFicha, // no tener en cuenta margen izquierdo para inicio del dibujo
+          (py - margenSuperior) * altoFicha, // no tener en cuenta margen superior para inicio del dibujo
+          anchoFicha,
+          altoFicha
+        );
+      }
+    }
+  }
+}
 
 // Leer el teclado
 function inicializaTeclado() {
@@ -338,5 +400,6 @@ function borraCanvas() {
 
 function principal() {
   borraCanvas();
+  dibujaTablero();
   pieza.dibuja();
 }
